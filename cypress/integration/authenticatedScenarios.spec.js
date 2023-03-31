ed9849421e14fefec6a0d3d548eaa3f246af3441
Felipe Blanco <feeh.blanco@gmail.com>
2023-03-30 22:59:36 -0300

    Fix lint issue

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -1,43 +1,45 @@
 describe("Scenarios where authentication is a pre-requirement", () => {
   beforeEach(() => {
-    cy.intercept("GET", "**/notes").as("getNotes");
-    cy.login();
-  });
+    cy.intercept("GET", "**/notes").as("getNotes")
+    cy.login()
+  })
 
   it("CRUDs a note", () => {
-    const faker = require("faker");
-    const noteDescription = faker.lorem.words(4);
+    const faker = require("faker")
+    const noteDescription = faker.lorem.words(4)
 
-    cy.createNote(noteDescription);
-    cy.wait("@getNotes");
+    cy.createNote(noteDescription)
+    cy.wait("@getNotes")
 
-    const updatedNoteDescription = faker.lorem.words(4);
-    const attachFile = true;
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
 
-    cy.editNote(noteDescription, updatedNoteDescription, attachFile);
-    cy.wait("@getNotes");
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait("@getNotes")
 
-    cy.deleteNote(updatedNoteDescription);
-    cy.wait("@getNotes");
-  });
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait("@getNotes")
+  })
 
   it("successfully submits the form", () => {
-    cy.intercept("POST", "**/prod/billing").as("paymentRequest");
+    cy.intercept("POST", "**/prod/billing").as("paymentRequest")
 
-    cy.fillSettingsFormAndSubmit();
+    cy.fillSettingsFormAndSubmit()
 
-    cy.wait("@getNotes");
+    cy.wait("@getNotes")
     cy.wait("@paymentRequest").then((response) => {
-      expect(response.state).to.equal("Complete");
-    });
-  });
+      expect(response.state).to.equal("Complete")
+    })
+  })
   it("logs out", { tags: "@desktop-and-tablet" }, () => {
-    cy.visit("/");
-    cy.wait(2000);
+    cy.visit("/")
+    cy.wait("@getNotes")
+
+
 
     /* ==== Generated with Cypress Studio ==== */
-    cy.get(".nav > :nth-child(2) > a").click();
-    cy.get("#email").should("be.visible");
+    cy.get(".nav > :nth-child(2) > a").click()
+    cy.get("#email").should("be.visible")
     /* ==== End Cypress Studio ==== */
-  });
-});
+  })
+})
